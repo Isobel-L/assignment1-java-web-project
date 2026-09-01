20024e24524ecf60b987ffc4b03fbc1520468366
Isobel-L <isobel.leviton@student.adelaide.edu.au>
2026-09-01 13:00:32 +0930

    initialise Spring Boot Maven project

---
+++ src/main/java/com/assignment/speechtotext/Assignment1JavaWebProjectApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.speechtotext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment1JavaWebProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Assignment1JavaWebProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assignment/speechtotext/Assignment1JavaWebProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.speechtotext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignment1JavaWebProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
